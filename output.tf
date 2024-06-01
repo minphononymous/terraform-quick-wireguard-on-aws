7e5cd1a3f51c88de17d2fb81fd9b289bdfa61be9
minphononymous <59946658+minphononymous@users.noreply.github.com>
2024-06-01 22:35:42 +0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,20 @@
 output "public_ip" {
   description = "The Public IP address used to access the instance"
-  value       = aws_instance.wiregurad.public_ip
+  value       = aws_instance.wireguard.public_ip
 }
 
 output "ssh_key" {
     description = "SSH keypair to access the instance"
     value = aws_key_pair.kp.key_name  
 }
-/*
+
 output "connect_to_wireguard_server" {
   description = "Login to the wireguard to generate the wireguard config."
   value       = <<-EOT
                 Use the following command to login into wireguard
-
-                ssh -i ubuntu@${aws_instance.wireguard.public_ip}
+                $ ssh -i ${aws_key_pair.kp.key_name}.pem ubuntu@${aws_instance.wireguard.public_ip}
+                
+                Use the following command to import wireguard by QR. (Use peer 1 - 5 for multiple devices)
                 cd /opt/wireguard-server/config/peer1 && qrencode -t ansiutf8 < peer1.conf
 
                 You should wait till all instances are fully ready in the EC2 console.
@@ -21,4 +22,3 @@ output "connect_to_wireguard_server" {
 
                 EOT
 }
-*/
